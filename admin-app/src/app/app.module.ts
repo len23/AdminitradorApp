b2a1b4ec795f590945e9594aff708e52ecd23908
Unknown <lenin.montalvo.23@hotmail.com>
2018-09-18 12:12:52 -0500

    notificaciones

--- admin-app/src/app/app.module.ts
+++ admin-app/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { NotificacionesComponent } from './notificaciones/notificaciones.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    NotificacionesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
